4e574099db6ebe6dccedad29f7d6c03eb30321ad
LGP <LGP@DESKTOP-JVHQ09R>
2024-12-27 02:54:12 +0800

    添加项目文件。

---
+++ 06整型.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main6() {
+
+//整型
+//1.短整型 (-32768~32767)
+	short num1 = 10;
+//2.整型 (-2147483648~21474836747)
+		int num2 = 10;
+//3.长整型 (-2147483648~21474836747)
+		long num3 = 10;
+			//4.长长整型 （-9,223,372,036,854,775,808~9,223,372,036,854,775,807）
+			long long num4 = 10;
+
+			cout << "num1=" << num1 << endl;
+			cout << "num2=" << num2 << endl;
+			cout << "num3=" << num3 << endl;
+			cout << "num4=" << num4 << endl;
+	system("pause");
+
+	return 0;
+}
