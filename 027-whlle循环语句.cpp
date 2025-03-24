2d0f4a500590a5516cab6e66f9276598fcb7f0d6
光鹏 梁 <750243132@qq.com>
2025-03-24 12:40:21 +0800

    1

--- 027-whlle循环语句.cpp
+++ 027-whlle循环语句.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+int main27() {
 
 	//while循环
-	//
+	//在屏幕打印0~9这十个数字
+	/*int num = 0;
+	cout << num << endl;
+	num++;*/
+	int num = 0;
+	//whilhe(循环条件)
+	while (num < 10) {
+		cout << num << endl;
+		num++;
+	}
+	
 
 	system("pause");
 
