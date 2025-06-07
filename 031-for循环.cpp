c3100e84b60b7dd0b8d795410081de2e6d9913f3
光鹏 梁 <750243132@qq.com>
2025-06-07 22:11:33 +0800

    1

---
+++ 031-for循环.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main31() {
+
+	//for循环
+	//从数字0打印到9
+	for (int i = 0; i < 10; i++) {
+		cout << i << endl;
+	}
+
+
+
+	system("pause");
+
+	return 0;
+
+}
---
+++ 032-敲桌子.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main32() {
+
+
+	for (int num1 = 1; num1 <= 100; num1++) {
+		if (num1 % 7 == 0 || num1 / 10 % 10 == 7 || num1 / 10 == 7) {
+
+			cout << "ÇÃ×À×Ó" <<  num1 <<endl;
+		}
+
+
+	}
+
+
+
+
+	system("pause");
+
+	return 0;
+
+
+
+}
